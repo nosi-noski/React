955406bf741d5eb54bc67fd1fd13012e3e24711d
nosi-noski <nosi-noski@mail.ru>
2020-11-09 20:58:35 +0300

    12 lesson: little grid, first component

---
+++ IT_Kamasutra/04_create-react-app/src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div className="bck-image"></div>
+            <div>
+                <div className="user-logo"></div>
+                <div className="user-description">description</div>
+            </div>
+            <div className="user-feed">
+                
+                <div className="my-post">my-post
+                    <div className="new-post">new-post</div>
+                    <div className="posts">posts</div>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Profile;
