656facc857bb76a6d01c8effc61880c76f0ed7f9
nosi-noski <nosi-noski@mail.ru>
2020-12-10 21:53:39 +0300

    22 - Route exact

--- IT_Kamasutra/04_create-react-app/src/components/Dialogs/Dialogs.jsx
+++ IT_Kamasutra/04_create-react-app/src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import classes from'./Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
-    return (
-        <div className={classes.main}>
-            Dialogs
-        </div>
-    );
+	return (
+		<div className={classes.dialogs}>
+			<div className={classes.dialogsItems}>
+                <NavLink 
+                    to="/dialogs/1" 
+                    className={classes.dialog}
+                    activeClassName={classes.activeLink}>
+                        Ivan
+                </NavLink>
+                <NavLink 
+                    to="/dialogs/2" 
+                    className={classes.dialog}
+                    activeClassName={classes.activeLink}>
+                        Sergey
+                </NavLink>
+                <NavLink 
+                    to="/dialogs/3" 
+                    className={classes.dialog}
+                    activeClassName={classes.activeLink}>
+                        Stas
+                </NavLink>
+                <NavLink 
+                    to="/dialogs/4" 
+                    className={classes.dialog}
+                    activeClassName={classes.activeLink}>
+                    Nataliya
+                </NavLink>
+			</div>
+					
+			<div className={classes.messages}>
+				<div className={classes.message}>Hi</div>
+				<div className={classes.message}>How are you?</div>
+				<div className={classes.message}>Have you ever coded in React?</div>
+				<div className={classes.message}>I have special offer for you)) Text me back, please)</div>
+			</div>
+		</div>
+	);
 }
 
 export default Dialogs;
--- IT_Kamasutra/04_create-react-app/src/components/NavBar/NavBar.jsx
+++ IT_Kamasutra/04_create-react-app/src/components/NavBar/NavBar.jsx
@@ -2,60 +2,62 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import classes from'./NavBar.module.css';
 
+
 const NavBar = () => {
-    return (
-        <nav className={classes.nav}>
+	return (
+		<nav className={classes.nav}>
 
-            <div className={classes.item}>
-                <NavLink 
-                    to="/profile"
-                    activeClassName={classes.activeLink}>
-                        Profile 
-                </NavLink>
-            </div>
+			<div className={classes.item}>
+				<NavLink 
+					to="/profile"
+					activeClassName={classes.activeLink}>
+						Profile
+				</NavLink>
+			
+			</div>
+		
+			<div className={classes.item}>
+				<NavLink 
+					to="/dialogs" 
+					activeClassName={classes.activeLink}>
+						Dialogs
+				</NavLink>
+			</div>
 
-            <div className={classes.item}>
-                <NavLink 
-                    to="/dialogs" 
-                    activeClassName={classes.activeLink}>
-                        Dialogs
-                </NavLink>
-            </div>
+			<div className={classes.item}>
+				<NavLink 
+					to="/messages"                    
+					activeClassName={classes.activeLink}>
+						Messages
+				</NavLink>
+			</div>
 
-            <div className={classes.item}>
-                <NavLink 
-                    to="/messages"                    
-                    activeClassName={classes.activeLink}>
-                        Messages
-                </NavLink>
-            </div>
+			<div className={classes.item}>
+				<NavLink 
+					to="/news"
+					activeClassName={classes.activeLink}>
+						News
+				</NavLink>
+			</div>
 
-            <div className={classes.item}>
-                <NavLink 
-                    to="/news"
-                    activeClassName={classes.activeLink}>
-                        News
-                </NavLink>
-            </div>
+			<div className={classes.item}>
+				<NavLink 
+					to="/music"
+					activeClassName={classes.activeLink}>
+						Music
+					</NavLink>
+			</div>
 
-            <div className={classes.item}>
-                <NavLink 
-                    to="/music"
-                    activeClassName={classes.activeLink}>
-                        Music
-                    </NavLink>
-            </div>
-
-            <div className={classes.item}>
-                <NavLink 
-                    to="/settings"
-                    activeClassName={classes.activeLink}>
-                    Settings
-                </NavLink>
-            </div>
-            
-        </nav>
-    );
+			<div className={classes.item}>
+				<NavLink 
+					to="/settings"
+					activeClassName={classes.activeLink}>
+					Settings
+				</NavLink>
+			</div>
+			
+		</nav>
+	);
 }
 
 export default NavBar;
