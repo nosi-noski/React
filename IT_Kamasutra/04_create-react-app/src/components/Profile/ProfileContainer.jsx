ccc6587fb00e3955e7518ce0080ca25fdba56915
nosi-noski <nosi-noski@mail.ru>
2021-03-24 17:01:19 +0300

    80 - init app

--- IT_Kamasutra/04_create-react-app/src/components/Profile/ProfileContainer.jsx
+++ IT_Kamasutra/04_create-react-app/src/components/Profile/ProfileContainer.jsx
@@ -19,7 +19,7 @@ class ProfileContainer extends React.Component {
             
             userId = this.props.authorizedUserId;
             if( !userId ){
-                //this.props.history.push('/login')
+                this.props.history.push('/login')
             }
         }
         // --====== 1 Step =======
